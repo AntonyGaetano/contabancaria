61cbce3d79aae41b387684b621e7a0edc1f803d5
Antony Gaetano <86926752+AntonyGaetano@users.noreply.github.com>
2021-09-29 08:56:55 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -3,18 +3,10 @@ import Header from './Header';
 import Main from "./Main"
 import "./App.css"
 
-var style_app = {
-  display:"flex",
-  flexDirection:"column",
-  width:"100%",
-  justifyContent:"center",
-  alignItems:"center",
-  
-}
 
 function App() {
   return (
-   <section style={style_app}>
+   <section className="app">
      <Header />
      <Main/>
    </section>
--- src/Header.js
+++ src/Header.js
@@ -2,21 +2,11 @@ import React from "react";
 import SaldoAtual from "./SaldoAtual";
 import "./Header.css"
 
-var styleHeader = {
-    display:"flex", 
-    width:"70%", 
-    height:"220px",
-    backgroundColor:"#737373",
-    border:"1px solid black",
-    color:"white",
-    justifyContent:"space-around",
-    alignItems:"center",
-}
 
 export default function Header(){
 
     return(
-        <section style={styleHeader}>
+        <section className="header">
           <h3 style={{width:"150px", height:"150px", borderRadius:"50%", backgroundColor:"#ccc"}}></h3>
           <SaldoAtual/>
         </section>
