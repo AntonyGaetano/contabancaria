43013e8d79a48c10fce6aaeeb04b2cf6a24a6243
Antony Gaetano <86926752+AntonyGaetano@users.noreply.github.com>
2021-09-30 17:30:33 -0300

    Update SaldoAtual.js

--- src/SaldoAtual.js
+++ src/SaldoAtual.js
@@ -2,16 +2,19 @@
 import React from "react";
 import "./SaldoTotal.css"
 
+var saldoAtual = 0;
+
 export default function SaldoAtual(props){
 
-    const SaldoAtual = props.saldo; 
+
+    saldoAtual += Number(props.saldo); 
 
     return(
 
     <div className="saldo">
       <h2>Olá, seja bem vindo!</h2>
       <h4>Seu saldo atual</h4>
-      <p style={{color:"green", backgroundColor:"white", padding:"0.0rem 0.0rem", width:"110px", margin:"0.7rem auto", borderRadius:"6px"}}> {"R$ " + SaldoAtual} </p>
+      <p style={{color:"green", backgroundColor:"white", padding:"0.0rem 0.0rem", width:"110px", margin:"0.7rem auto", borderRadius:"6px"}}> {"R$ " + saldoAtual} </p>
     </div>
 
     )
